26ff08a3249534c3f5f72c8d89e1a9e413b31d28
JayantiTA <jayjayanti18@gmail.com>
2021-12-11 01:17:16 +0700

    feat: add data time class

---
+++ src/com/TCourse/Manager/DataTime.java
@@ -0,0 +1,10 @@
+package com.TCourse.Manager;
+
+public class DataTime {
+  
+  public static long time;
+  
+  public static void setTime(long t) { time = t; }
+  public static long gettime() { return time; }
+  
+}
